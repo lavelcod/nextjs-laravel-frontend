ced6b5e8b8958ce5c373e8e64361fb97d576fd8b
lavelcod <lavelcod@gmail.com>
2025-08-30 11:15:08 +0330

    Lec 11-6 mobile menu

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -10,7 +10,7 @@ function Menu() {
   // console.log(pathName)
   const [fixedHeader, setFixedHeader] = useState(false);
   const [openMobileMenu, setOpenMobileMenu] = useState(false);
-  console.log(openMobileMenu)
+  
   return (
     <>
       <div className={`header-menu bg-white  ${fixedHeader ? "fixed" : ""}`}>
@@ -119,8 +119,37 @@ function Menu() {
         <div id="menu-mobile-block" className={`${openMobileMenu && "open"}`}>
           <div className="menu-mobile-main">
             <div className="container">
-              <ul>
-                <li>Home</li>
+              <ul className="menu-nav-mobile h-full pt-1 pb-1">
+                <li className="nav-item-mobile flex-column gap-2 pt-2 pl-3 pr-3 pb-2 pointer">
+                  <a className="nav-link-mobile flex items-center justify-between" href="/">
+                    <span className="body2 font-bold">Home</span>
+                  </a>
+                </li> 
+                <li className="nav-item-mobile flex-column gap-2 pt-2 pl-3 pr-3 pb-2 pointer">
+                  <a className="nav-link-mobile flex items-center justify-between" href="/about">
+                    <span className="body2 font-bold">About us</span>
+                  </a>
+                </li> 
+                <li className="nav-item-mobile flex-column gap-2 pt-2 pl-3 pr-3 pb-2 pointer">
+                  <a className="nav-link-mobile flex items-center justify-between" href="/service">
+                    <span className="body2 font-bold">Our Services</span>
+                  </a>
+                </li> 
+                <li className="nav-item-mobile flex-column gap-2 pt-2 pl-3 pr-3 pb-2 pointer">
+                  <a className="nav-link-mobile flex items-center justify-between" href="/case-studies">
+                    <span className="body2 font-bold">Case Studies</span>
+                  </a>
+                </li> 
+                <li className="nav-item-mobile flex-column gap-2 pt-2 pl-3 pr-3 pb-2 pointer">
+                  <a className="nav-link-mobile flex items-center justify-between" href="/blog">
+                    <span className="body2 font-bold">Blog</span>
+                  </a>
+                </li> 
+                <li className="nav-item-mobile flex-column gap-2 pt-2 pl-3 pr-3 pb-2 pointer">
+                  <a className="nav-link-mobile flex items-center justify-between" href="/contact">
+                    <span className="body2 font-bold">Contact us</span>
+                  </a>
+                </li> 
               </ul>
             </div>
           </div>
