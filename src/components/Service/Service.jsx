86ad540cee10bac0cba396879493935a74ee6e2b
lavelcod <lavelcod@gmail.com>
2025-09-01 06:50:20 +0330

    Lec 13-2 creating section services Component

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -9,7 +9,7 @@ function Service({data}) {
   const isInView = useInView(ref, {once: true})
   return (
     <div>
-      <section className='service-block lg:mt-[100px] sm:mt-16 m-10'>
+      <section className='service-block lg:mt-[100px] sm:mt-16 m-10' ref={ref}>
         <div className='container'>
           <h3 className='heading3 text-center'>Our Services</h3>
           <div className='list-service grid lg:grid-cols-3 sm:grid-col-2 gap-8 md:mt-10 mt-6 gap-y-10 ' style={{ transform : isInView ? "none" : "translateY(60px)" , opacity: isInView ? 1 : 0, transition: 'all 0.7s cubic-bezier(0.17, 0.55, 0.55,1) 0.3s'}}>
--- src/components/Service/item/ServiceItem.jsx
+++ src/components/Service/item/ServiceItem.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-function ServiceItem({data}) {
+function ServiceItem({ data, number }) {
   return (
-    <div>ServiceItem</div>
-  )
+    <div className="service-item p-8 bg-white rounded-lg border border-line hover-box-shadow">
+      <Link className="service-item-main h-full" href={"/"}>
+        <div className="heading flex items-center justify-between">
+          <i className={`${data.icon} text-blue md:text-6xl text-5xl`}></i>
+          <div className="number heading3 text-placehover text-slate-400">
+            {number + 1}
+          </div>
+        </div>
+        <div className="heading6 hover:text-blue duration-300 mt-6">
+          {data.title}
+        </div>
+        <div className="text-secondary mt-2">{data.desc}</div>
+      </Link>
+    </div>
+  );
 }
 
-export default ServiceItem
+export default ServiceItem;
