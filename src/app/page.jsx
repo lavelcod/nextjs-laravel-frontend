b714adda67d5d2e02190ad8ec074e863bc5cc3d9
lavelcod <lavelcod@gmail.com>
2025-09-02 08:17:28 +0330

    Lec 14 payment gateway section in Homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import Slider from "@/components/Slider/Slider";
 import Image from "next/image";
 import Service from "../components/Service/Service";
 import serviceData from "@/data/service.json"
+import PaymentGateway from "@/components/PaymentGateway/PaymentGateway";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <main className="content">
         <Slider />
         <Service data={serviceData} />
+        <PaymentGateway/>
       </main>
     </div>
   );
